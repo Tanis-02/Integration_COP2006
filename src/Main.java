7c45dee5fc00e7bd9ebd6b5b53735648d34d74d7
Tanis-02 <egonzalez.1627@gmail.com>
2019-01-31 15:44:46 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+// Elizabeth Gonzalez
+// A program integrating skills learned in COP 2006. My theme for my integration project is my
+// Dungeons and Dragons campaign. It will include character bios for each of the
+// characters, their spells/weapons and different fun facts about them, all written in the voice of
+// my own character Keros.
+
+// CTRL + SHIFT + F : to format code
+
+public class Main {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    System.out.println(
+        "Hiya! My name is Keros and I'd like to introduce you to my new adventurer friends Elly, Heian, Kadreus and Hanz!");
+
+    // Rule: any time you go from numbers to string you have to clear out the input string
+
+
+
+  }
+}
